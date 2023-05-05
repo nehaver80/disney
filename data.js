f4e8b472dac8fead509f86aa2be9efd878de1da1
nehaver80 <nainaver3045@gmail.com>
2023-05-05 11:34:18 +0530

    Intial commit

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+  {
+    name: 'Brahmastra: Part One Shiva',
+    des: 'A young man on the brink of falling in love gets his world turned upside down when he discovers he has the power to control fire and a connection to a secret society of guardians',
+    image: 'images/slider 2.PNG'
+  },
+  {
+    name: 'willow',
+    des: 'An unlikely group of heroes set off on a dangerous quest to places far beyond their home, where they must face their inner demons and come together to save their world.',
+    image: 'images/slider 1.PNG'
+  },
+  {
+    name: 'wanda vision',
+    des: 'Living idealized suburban lives, super-powered beings Wanda and Vision begin to suspect that everything is not as it seems.',
+    image: 'images/slider 3.PNG'
+  },
+  {
+    name: 'loki',
+    des: 'Loki, the God of Mischief, steps out of his brothers shadow to embark on an adventure that takes place after the events of Avengers Endgame.',
+    image: 'images/slider 4.PNG'
+  },
+  {
+    name: 'luca',
+    des: 'Luca, a sea monster, befriends Alberto, another one of his kind who takes him on a land adventure. He experiences an exciting summer while keeping his parents in the dark.',
+    image: 'images/slider 5.PNG'
+  }
+];
